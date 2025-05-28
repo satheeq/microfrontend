939780177791120ad0adefd33c35a84607b6563f
h_satheeq <h.satheeq@directfn.com>
2025-05-28 11:55:16 +0530

    header added

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter} from "react-router-dom";
+
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return <div>
-      <h1>Hello World from Container!</h1>
-      <hr/>
-      <MarketingApp/>
+      <BrowserRouter>
+          <div>
+              <Header/>
+              <MarketingApp/>
+          </div>
+      </BrowserRouter>
   </div>;
 }
