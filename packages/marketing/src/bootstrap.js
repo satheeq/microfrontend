f495dfaf391619de300c96536076f5de819030e2
h_satheeq <h.satheeq@directfn.com>
2025-05-28 18:30:23 +0530

    browser history and memory history sync between container & marketing app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createMemoryHistory, createBrowserHistory} from 'history';
 import App from './App';
 
-const mount = (el) => {
-    ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate); // Notify the container on navigation changes
+    }
+
+    ReactDOM.render(<App history={history}/>, el);
+    
+    return {
+        onParentNavigate: ({ pathname: nextPathName}) => {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathName) {
+                console.log('parent navigating to ', nextPathName);
+                history.push(nextPathName);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {defaultHistory: createBrowserHistory()});
     }
 }
 
