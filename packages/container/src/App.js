72c1639eafac249d1c7b8af38d5b12a80ab99a73
h_satheeq <h.satheeq@directfn.com>
2025-05-29 18:45:01 +0530

    authentication and logout changes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, {lazy, Suspense} from 'react';
+import React, {lazy, Suspense, useState} from 'react';
 import { BrowserRouter, Route, Switch} from "react-router-dom";
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
@@ -13,19 +13,23 @@ const generateClassName = createGenerateClassName({
 });
 
 export default () => {
-  return <div>
-      <BrowserRouter>
-          <StylesProvider generateClassName={generateClassName}>
-              <div>
-                  <Header/>
-                  <Suspense fallback={<Progress />}>
-                      <Switch>
-                          <Route path="/auth" component={AuthAppLazy}/>
-                          <Route path="/" component={MarketingAppLazy}/>
-                      </Switch>
-                  </Suspense>
-              </div>
-          </StylesProvider>
-      </BrowserRouter>
-  </div>;
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
+    return <div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)}/>
+                    <Suspense fallback={<Progress/>}>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthAppLazy onSignIn={() => setIsSignedIn(true)}/>
+                            </Route>
+                            <Route path="/" component={MarketingAppLazy}/>
+                        </Switch>
+                    </Suspense>
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
+    </div>;
 }
